dcbc3a9ed6808a05bec6f34bb626c43832c21e5b
harperaustin <144397087+harperaustin@users.noreply.github.com>
2024-12-14 17:30:20 +0000

    created my own heuristic

--- agents.py
+++ agents.py
@@ -9,6 +9,7 @@ from adversarial_search import minimax, alpha_beta
 import pickle
 import torch
 from torch import nn
+from typing import Dict, Tuple
 
 
 MAXIMIZER = 0
@@ -136,6 +137,132 @@ class AlphaBetaAgent(GameAgent):
 
     def __str__(self):
         return f"AlphaBeta w/ depth {self.depth} + " + str(self.search_problem)
+    
+class AlphaBetaAgentDiff(GameAgent):
+    def __init__(self, depth=1, search_problem=GoProblemMerpHeuristic()):
+        super().__init__()
+        self.depth = depth
+        self.search_problem = search_problem
+
+    def get_move(self, game_state: GoState, time_limit: float) -> Action:
+        """
+        Get move of agent for given game state using alpha-beta algorithm
+
+        Args:
+            game_state (GameState): current game state
+            time_limit (float): time limit for agent to return a move
+        Returns:
+            best_action (Action): best action for current game state
+        """
+        
+
+        def alpha_beta_diff(asp: self.search_problem, game_state: GoState, start_time, time_limit, cutoff_depth=float('inf')) -> Tuple[Action, Dict[str, int]]:
+            """
+            Implement the alpha-beta pruning algorithm on ASPs,
+            assuming that the given game is both 2-player and constant-sum.
+
+            Input:
+                asp - an AdversarialSearchProblem
+                cutoff_depth - the maximum search depth, where 0 is the start state,
+                            Depth 1 is all the states reached after a single action from the start state (1 ply).
+                            cutoff_depth will always be greater than 0.
+            Output:
+                an action (an element of asp.get_available_actions(asp.get_start_state()))
+                a dictionary of statistics for visualization
+                    states_expanded: stores the number of states expanded during current search
+                                    A state is expanded when get_available_actions(state) is called.
+            """
+            def val_ab(state: GameState, problem: self.search_problem, depth, alpha, beta):
+                # Base cases:
+                # if the problem is terminal, return the reward
+                if problem.is_terminal_state(state):
+                    return (problem.evaluate_terminal(state), None)
+                # if reached cutoff depth, return the heuristic value
+                elif depth == cutoff_depth:
+                    if problem.heuristic(state, state.player_to_move()) == None:
+                        print("heuristic is returning none.\n")
+                    return (problem.heuristic(state, state.player_to_move()), None)
+                
+                else:
+                    # MAX TURN
+                    stats['states_expanded'] += 1
+                    if state.player_to_move() == 0:
+                        return max_val_ab(state, problem, depth, alpha, beta)
+                    else:
+                        # MIN TURN
+                        return min_val_ab(state, problem, depth, alpha, beta)
+
+            def max_val_ab(state: GameState, problem: self.search_problem, depth, alpha, beta):
+                # start with a really low value
+                max_v = float('-inf')
+                best_action = None
+                # look at every possible action from the current state
+                if time.time() - start_time >= time_limit:
+                    return (max_v, best_action)
+                for action in problem.get_available_actions(state):
+                    next_state = problem.transition(state, action)
+                    # recursively call val on every next state
+                    curr_v = val_ab(next_state, problem, depth + 1, alpha, beta)[0]
+                    # if this val is greater than the max val, then set the new value and action
+                    # as the current best
+                    if curr_v >= max_v:
+                        max_v = curr_v
+                        best_action = action
+
+                    alpha = max(alpha, max_v)
+                    # checking if in this branch the minimizer already has an option
+                    # that is lower than the current alpha. If so, we can prune, because
+                    # the maximizer will only choose higher values
+                    if beta <= alpha:
+                        break
+                # return the max v and the best action
+                return (max_v, best_action)
+            
+            def min_val_ab(state: GameState, problem: self.search_problem, depth, alpha, beta):
+                # start with a really high value
+                min_v = float('inf')
+                best_action = None
+                if time.time() - start_time >= time_limit:
+                    return (min_v, best_action)
+                # look at every possible action from the current state
+                for action in problem.get_available_actions(state):
+                    next_state = problem.transition(state, action)
+                    # recursively call val on every next state
+                    curr_v = val_ab(next_state, problem, depth + 1, alpha, beta)[0]
+                    if curr_v <= min_v:
+                        # if this val is less than the min val, then set the new value and action
+                        # as the current best
+                        min_v = curr_v
+                        best_action = action
+                    beta = min(beta, min_v)
+                        # checking if in this branch the maximizer already has an option
+                        # that is higher than the current beta. If so, we can prune, because
+                        # the minimizer will only choose lower values.
+                    if beta <= alpha:
+                        break
+                # return the min v and the best action
+                return (min_v, best_action)
+            
+
+
+            stats = {
+                'states_expanded': 0  # Increase by 1 for every state transition
+            }
+
+            _,best_action = val_ab(game_state, asp, 0, float('-inf'), float('inf'))
+            return best_action, stats
+            
+
+        start_time = time.time()
+        best_action, _ = alpha_beta_diff(self.search_problem, game_state, start_time, time_limit, cutoff_depth=self.depth)
+        return best_action
+
+
+    def __str__(self):
+        return f"AlphaBeta Diff w/ depth {self.depth} + " + str(self.search_problem)
+    
+
+
 
 
 class IterativeDeepeningAgent(GameAgent):
@@ -764,7 +891,7 @@ class GoProblemCustomHeuristic(GoProblem):
         Output:
             value: heuristic (value) of current state
         """
-        
+        # MAY NEED AN IF STATEMENT HERE, like if end game heuristic is alpha beta, 
         value = 0
         # get encoding for the state:
         state_encoding = self.encoding(state)
@@ -776,6 +903,24 @@ class GoProblemCustomHeuristic(GoProblem):
         # Note, your agent may perform better if you force it not to pass
         # (i.e., don't select action #25 on a 5x5 board unless necessary)
         return value
+    
+    def get_move(self, game_state, time_limit=1):
+      """
+      Get best action for current state using self.model
+
+      Input:
+        game_state: current state of the game
+        time_limit: time limit for search (This won't be used in this agent)
+      Output:
+        action: best action to take
+      """
+
+      # if we are at opening book place, then use opening book aciton
+      # if mid game, use learned value function
+      # if we are end game, use alpha beta with better heuristic
+      return None
+    
+
 
     def __str__(self) -> str:
         return "Merp Agent"
@@ -881,8 +1026,8 @@ def get_features_advanced(game_state: GoState):
 
 def main():
     from game_runner import run_many
-    agent1 = create_value_agent_from_model()
-    agent2 = create_custom_agent_from_model()
+    agent1 = AlphaBetaAgentDiff(depth=2)
+    agent2 = create_value_agent_from_model()
     
     # Play 10 games
     run_many(agent1, agent2, 40)
--- heuristic_go_problems.py
+++ heuristic_go_problems.py
@@ -1,4 +1,4 @@
-from go_search_problem import GoProblem
+from go_search_problem import GoProblem, GoState
 BLACK = 0
 WHITE = 1
 
@@ -31,3 +31,66 @@ class GoProblemLearnedHeuristic(GoProblem):
 
     def __str__(self) -> str:
         return "Learned Heuristic"
+    
+class GoProblemMerpHeuristic(GoProblem):
+    def __init__(self, model=None, state=None,):
+        super().__init__(state=state)
+        self.model = model
+
+    def encoding(self, state):
+        pass
+
+    def heuristic(self, state, player_index):
+        black_territory, white_territory = 0, 0
+        visited = set()
+        board = state.get_board()
+
+
+        # This gets the size of connected regions of the board
+        def flood_fill(x,y, state, color):
+            stack = [(x,y)]
+            region = []
+            liberties = set()
+            curr_player_pieces = state.get_pieces_array(color)
+           
+            empty_spaces = state.get_board()[2]
+
+            while stack:
+                curr_x, curr_y = stack.pop()
+                if (curr_x, curr_y) in visited:
+                    continue
+                visited.add((curr_x,curr_y))
+                region.append((curr_x, curr_y))
+
+                for neighbor_x, neighbor_y in neighbors(curr_x, curr_y):
+                    if curr_player_pieces[neighbor_x][neighbor_y] == 1:
+                        stack.append((neighbor_x, neighbor_y))
+                    elif empty_spaces[neighbor_x][neighbor_y] == 1:
+                        liberties.add((neighbor_x, neighbor_y))
+            
+            return region, liberties
+
+        
+        def neighbors(x, y):
+            # get all connecting game states that are in the board
+            ns = [(x - 1, y), (x + 1, y), (x, y - 1), (x , y + 1)]
+            return [(x, y) for x, y in ns if 0 <= x < 5 and 0 <= y < 5]
+        
+
+        # loop through every slot in the board
+        for x in range(5):
+            for y in range(5):
+                # only visit each slot once
+                if (x,y) not in visited:
+                    if board[BLACK][x][y] == 1:
+                        region, liberties = flood_fill(x,y,state, BLACK)
+                        black_territory += len(liberties)
+                    elif board[WHITE][x][y] == 1:
+                        region, liberties = flood_fill(x,y,state, WHITE)
+                        white_territory += len(liberties)
+
+        return black_territory - white_territory
+
+
+    def __str__(self) -> str:
+        return "Merp Heuristic"
